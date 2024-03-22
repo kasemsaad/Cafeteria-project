542eba2e644ad6cbe5c490afbdc730fd6a397a76
kasemsaad <kasemsaad756@gmail.com>
2024-03-22 15:42:07 +0200

    create customer schema

--- connection.php
+++ connection.php
@@ -1,6 +1,3 @@
-
-
-
 <?php
 ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
@@ -11,21 +8,39 @@ class db
     private $server = 'localhost';
     private $username = 'root'; 
     private $password = '';
-    private $database = 'Cafeterie';
+    private $database = 'cafeteria';
     private $connection;
     function __construct()
     {
         try {
-            "CREATE DATABASE IF NOT EXISTS Cafeterie";
-            $dsn = "mysql:host={$this->server};dbname={$this->database}";
+            $dsn = "mysql:host={$this->server}";
             $this->connection = new PDO($dsn, $this->username, $this->password);
             $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            
+            $stmt = $this->connection->query("CREATE database if not exists {$this->database}");
+            $stmt->execute();
+
+            $this->connection->query("USE {$this->database}");
+
+            // insert schema
+            $CustomerTable = "CREATE table if not exists customers (
+                CID INT AUTO_INCREMENT PRIMARY KEY,
+                FirstName VARCHAR(50),
+                LastName VARCHAR(50),
+                Email VARCHAR(100),
+                Phone VARCHAR(20),
+                Address VARCHAR(255),
+                Password VARCHAR(255)
+            )";
+            $this->connection->query($CustomerTable);
+
+
+            
         } catch (PDOException $e) {
             die("Connection failed: " . $e->getMessage());
         }
     }
-
 }
-$db = new db(); // Create an instance of the db class and establish connection
-echo "Connected"; // If this is printed, it means the connection was successful
+$db = new db(); 
+echo "Connected"; 
 ?>
