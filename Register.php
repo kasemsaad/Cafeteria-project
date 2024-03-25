97650e540841bd8c87ebf4df3ffad25296fa5bac
kasemsaad <kasemsaad756@gmail.com>
2024-03-25 03:29:12 +0200

    valdiarion

--- Register.php
+++ Register.php
@@ -1,54 +1,10 @@
-<!-- <?php
-ini_set('display_errors', '1');
-ini_set('display_startup_errors', '1');
-error_reporting(E_ALL);
-
-// echo  $_GET['fname'];
-$err = [];
-if (isset($_GET['err'])) {
-  $err = json_decode($_GET['err'], true);
-}
-?>
-
-
-
-  <?php
-  if (isset($err['fname'])) {
-    echo "<span style='color:red'>$err[fname]</span> <br>";
-  }
-  ?>
-
-  Last name <input type="text" name="lname" placeholder="Last name"><br>
-  <?php
-  if (isset($err['lname'])) {
-    echo "<span style='color:red'>$err[lname]</span> <br>";
-  }
-  ?>
-  Address <textarea name="address"> </textarea> <br>
-  <?php
-  if (isset($err['address'])) {
-    echo "<span style='color:red'>$err[address]</span> <br>";
-  }
-  ?>
-
-  <?php
-  if (isset($err['email'])) {
-    echo "<span style='color:red'>$err[email]</span> <br>";
-  }
-  ?>
-  Password <input type="password" name="password" placeholder="Password"><br>
-  <?php
-  if (isset($err['password'])) {
-    echo "<span style='color:red'>$err[password]</span> <br>";
-  }
-  ?> -->
 <?php
-
-
 require 'connection.php';
 
-
-
+$err = [];
+if (isset ($_GET['err'])) {
+  $err = json_decode($_GET['err'], true);
+}
 
 ?>
 
@@ -82,21 +38,27 @@ require 'connection.php';
     top: 13px;
   }
 </style>
-<section class="vh-100 gradient-custom">
-  <div class="container py-5 h-100">
-    <div class="row justify-content-center align-items-center h-100">
+<section class=" gradient-custom">
+  <div class="container py-5">
+    <div class="row justify-content-center align-items-center">
       <div class="col-12 col-lg-9 col-xl-7">
         <div class="card shadow-2-strong card-registration" style="border-radius: 15px;">
           <div class="card-body p-4 p-md-5">
             <h3 class="mb-4 pb-2 pb-md-0 mb-md-5">Registration Form</h3>
-            <form action="CustomerControler.php" method="post"  enctype="multipart/form-data">
+            <form action="CustomerControler.php" method="post" enctype="multipart/form-data">
 
               <div class="row">
 
                 <div class="col-md-6 mb-4">
                   <div class="form-outline">
-                    <input type="text" id="firstName" class="form-control form-control-lg" name="FirstName" />
+                  <input type="text" id="firstName" class="form-control form-control-lg" name="FirstName"   />
                     <label class="form-label" for="firstName">First Name</label>
+
+                    <?php
+                    if (isset ($err['FirstName'])) {
+                      echo "<span style='color:red'>$err[FirstName]</span> <br>";
+                    }
+                    ?>
                   </div>
                 </div>
 
@@ -104,6 +66,12 @@ require 'connection.php';
                   <div class="form-outline">
                     <input type="text" id="lastName" class="form-control form-control-lg" name="LastName" />
                     <label class="form-label" for="lastName">Last Name</label>
+
+                    <?php
+                    if (isset ($err['LastName'])) {
+                      echo "<span style='color:red'>$err[LastName]</span> <br>";
+                    }
+                    ?>
                   </div>
                 </div>
 
@@ -116,15 +84,24 @@ require 'connection.php';
                   <div class="form-outline">
                     <input type="email" id="emailAddress" class="form-control form-control-lg" name="Email" />
                     <label class="form-label" for="email">Email</label>
+                    <?php
+                    if (isset ($err['Email'])) {
+                      echo "<span style='color:red'>$err[Email]</span> <br>";
+                    }
+                    ?>
                   </div>
                 </div>
 
 
                 <div class="col-md-6 mb-4 pb-2">
                   <div class="form-outline">
-                    <input type="tel" id="phoneNumber" name="Address" class="form-control form-control-lg"
-                      name="Address" />
-                    <label class="form-label" for="phoneNumber">Address</label>
+                    <input type="tel" id="Room" name="Room" class="form-control form-control-lg" name="Room" />
+                    <label class="form-label" for="phoneNumber">Room</label>
+                    <?php
+                    if (isset ($err['Room'])) {
+                      echo "<span style='color:red'>$err[Room]</span> <br>";
+                    }
+                    ?>
                   </div>
                 </div>
 
@@ -137,6 +114,11 @@ require 'connection.php';
                   <div class="form-outline datepicker w-100">
                     <input type="password" class="form-control form-control-lg" id="Password" name="Password" />
                     <label for="password" class="form-label">Password</label>
+                    <?php
+  if (isset($err['Password'])) {
+    echo "<span style='color:red'>$err[Password]</span> <br>";
+  }
+  ?>
                   </div>
                 </div>
 
@@ -144,6 +126,10 @@ require 'connection.php';
                   <div class="form-outline">
                     <input type="tel" id="phoneNumber" class="form-control form-control-lg" name="Phone" />
                     <label class="form-label" for="phoneNumber">Phone Number</label>
+                    <?php
+                    if (isset ($err['Phone'])) {
+                      echo "<span style='color:red'>$err[Phone]</span> <br>";
+                    }?>
                   </div>
                 </div>
 
@@ -152,12 +138,17 @@ require 'connection.php';
                     <input type="password" id="Confirm Password" class="form-control form-control-lg"
                       name="ConPassword" />
                     <label class="form-label" for="phoneNumber">Confirm Password</label>
+                    <?php
+                    if (isset($err['ConPassword'])) {
+    echo "<span style='color:red'>$err[ConPassword]</span> <br>";
+  }
+  ?>
                   </div>
                 </div>
 
                 <div class="col-md-6 mb-4 pb-2">
                   <div class="form-outline">
-                    <input type="file" id="profileImage" class="form-control form-control-lg" name="ProfileImage" />
+                    <input type="file" id="CustomerImage" class="form-control form-control-lg" name="CustomerImage" />
                     <label class="form-label" for="profileImage">Profile Image</label>
                   </div>
                 </div>
