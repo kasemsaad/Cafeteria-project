e8a7f97c87be3c058f4ee9b7e3705f09c3577d94
kasemsaad <kasemsaad756@gmail.com>
2024-03-30 02:24:20 +0200

    login Admin

--- index.php
+++ index.php
@@ -50,7 +50,7 @@ if (isset ($_GET['err'])) {
       <div class="col-12 col-lg-9 col-xl-7">
         <div class="card shadow-2-strong card-registration" style="border-radius: 15px;">
           <div class="card-body p-4 p-md-5">
-            <h3 class="mb-4 pb-2 pb-md-0 mb-md-5">login Cafetria</h3>
+            <h3 class="mb-4 pb-2 pb-md-0 mb-md-5">login by Admin Cafetria</h3>
             <form  method="post" action="CustomerControler.php" >
 
               <div class="row">
@@ -66,7 +66,7 @@ if (isset ($_GET['err'])) {
 
                 <div class="col-md-6 mb-4">
                   <div class="form-outline">
-                    <input type="text" name="Password" placeholder="Password" class="form-control form-control-lg" />
+                    <input type="password" name="Password" placeholder="Password" class="form-control form-control-lg" />
                     <label class="form-label" for="password">Password</label>
              
                   </div>
@@ -81,13 +81,14 @@ if (isset ($_GET['err'])) {
                }
                
                  ?>
-
+<a href="http://localhost/Cafeteria/forgetPassword.php">Forget Password</a>
           </div>
 
         </div>
         <div class="mt-4 pt-2">
 
           <input class="btn btn-primary btn-lg" type="submit" value="Submit" name="login"/>
+          
         </div>
 
         </form>
